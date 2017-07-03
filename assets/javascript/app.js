ead227b4d0efde35befa8f75fedc4d650d189302
MB DR <mderoberts@users.noreply.github.com>
2017-07-03 16:47:00 -0400

    log data in firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,33 @@ messagingSenderId: "717031140671"
 
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+// Initial variables
+var name = "";
+var destination = "";
+var time = "";
+var frequency = "";
+
+$("#submit").on("click", function(){
+    event.preventDefault();
+    // Get inputs
+    name = $("#name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    time = $("#time-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    // Object for new data
+    var newTrain = {
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency
+    };
+    // Add info to database
+    database.ref().push(newTrain);
+    // Empty inputs
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+});
