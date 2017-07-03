baa4eb2693809419efa9943b2a9d8d7c3aafac26
MB DR <mderoberts@users.noreply.github.com>
2017-07-03 15:48:10 -0400

    firebase initialization

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+apiKey: "AIzaSyAiOMYWcRPSFFuVB5laBFdgdu3vIwDE-0A",
+authDomain: "train-scheduler-b9579.firebaseapp.com",
+databaseURL: "https://train-scheduler-b9579.firebaseio.com",
+projectId: "train-scheduler-b9579",
+storageBucket: "",
+messagingSenderId: "717031140671"
+};
+
+firebase.initializeApp(config);
+
