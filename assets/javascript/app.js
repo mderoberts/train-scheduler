76b01c8937467b2043bf2453577c7192db09a5ba
MB DR <mderoberts@users.noreply.github.com>
2017-07-04 13:18:09 -0400

    final function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,7 @@ var destination = "";
 var time = "";
 var frequency = "";
 
+// Submit button to log info in firebase
 $("#submit").on("click", function(){
     event.preventDefault();
     // Get inputs
@@ -38,4 +39,33 @@ $("#submit").on("click", function(){
     $("#destination-input").val("");
     $("#time-input").val("");
     $("#frequency-input").val("");
+});
+
+// Firebase event to add new trains in database and HTML
+database.ref().on("child_added", function(snapshot){
+    console.log(snapshot.val());
+    // Store data
+    name = snapshot.val().name;
+    destination = snapshot.val().destination;
+    time = snapshot.val().time;
+    frequency = snapshot.val().frequency; 
+    // Console log data
+    console.log(name);
+    console.log(destination);
+    console.log(time);
+    console.log(frequency);
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(time, "hh:mm").subtract(1, "years");
+    // Current Time
+    var currentTime = moment();
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    // Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm A");
+    // Add data to table
+    $("#schedule-table > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>");
 });
